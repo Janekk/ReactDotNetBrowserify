ac8a19612e408755bdef951b799724116a84cc85
Janekk <jkacalak@gmail.com>
2014-11-19 00:44:01 +0100

    Fixed an issue with browserify bundle after updating to the newer browserify version (usage of browserify.require).

--- gulpfile.js
+++ gulpfile.js
@@ -20,90 +20,94 @@ var streams = require('memory-streams');
 var CombinedStream = require('combined-stream');
 var os = require('os');
 var createServerBundle = function (browserify, configPath) {
-  function parseConfig(config) {
-    if (config) {
-      if (config.expose) {
-        var components = {};
-        //1. parse the configuration
-        config.expose.forEach(function (component) {
-          var path, name;
+  var utils = {
+    parseConfig : function(config) {
+      if (config) {
+        if (config.expose) {
+          var components = {};
+          //1. parse the configuration
+          config.expose.forEach(function (component) {
+            var path, name;
 
-          if (typeof component === 'string') {
-            path = component;
-          }
-          else {
-            path = component.path;
-            if (component.name) {
-              name = component.name;
+            if (typeof component === 'string') {
+              path = component;
             }
-          }
-          if (name === undefined) {
-            var splitted = path.split('/');
-            name = splitted[splitted.length - 1];
-          }
-          components[name] = path;
-        });
-        return components;
+            else {
+              path = component.path;
+              if (component.name) {
+                name = component.name;
+              }
+            }
+            if (name === undefined) {
+              var splitted = path.split('/');
+              name = splitted[splitted.length - 1];
+            }
+            components[name] = path;
+          });
+          return components;
+        }
       }
+    },
+    exposeReact: function(exposedVariables, requires) {
+      requires.push({ file: "react" });
+      exposedVariables.append('var React = require("react");' + os.EOL);
     }
-  }
-
-  function exposeModule(exposedVariables, name, path) {
-    browserify.require(path, { expose: name });
-    exposedVariables.append('var ' + name + ' = require("' + name + '");' + os.EOL);
-  }
+  };
 
   if (configPath === undefined) {
     configPath = './reactServerConfig.json';
   }
   var config = require(configPath);
 
-  var serverComponents = parseConfig(config);
+  var serverComponents = utils.parseConfig(config);
   if (serverComponents) {
     var exposedVariables = CombinedStream.create();
+    var requires = [];
     exposedVariables.append(';' + os.EOL);
-    exposeModule(exposedVariables, 'React', 'react')
+    utils.exposeReact(exposedVariables, requires);
 
     for (var name in serverComponents) {
       var path = serverComponents[name];
-      browserify.add(path);
-      exposeModule(exposedVariables, name, path);
+      requires.push({file: path, expose: name});
+      exposedVariables.append('var ' + name + ' = React.createFactory(require("' + name + '"));' + os.EOL);
     }
-
+    browserify.require(requires);
     var bundleStream = CombinedStream.create();
     bundleStream.append(browserify.bundle());
     bundleStream.append(exposedVariables);
 
     return bundleStream;
   }
-}
-
-var react = require('gulp-react');
-gulp.task('react', function () {
-  return gulp.src(paths.src.jsx)
-    .pipe(react())
-    .pipe(gulp.dest(paths.dest.jsx));
-});
+};
 
 var browserify = require('browserify');
 var gulpServerBundle = function () {
-  var bundle = createServerBundle(browserify());
+  var bundle = createServerBundle(browserify(
+    {
+      extensions: ['.jsx', '.js']
+    }
+  ));
   return bundle
     .pipe(source(paths.dest.serverBundle))
     .pipe(gulp.dest(paths.dest.bundles));
-}
-gulp.task('server-build', ['react'], function () {
+};
+
+gulp.task('server-build', function () {
   return gulpServerBundle();
 });
 
 var gulpClientBundle = function () {
-  var b = browserify(paths.src.app);
+  var b = browserify(paths.src.app,
+    {
+      extensions: ['.jsx', '.js']
+    });
   var bundle = createServerBundle(b);
   return bundle
     .pipe(source(paths.dest.clientBundle))
     .pipe(gulp.dest(paths.dest.bundles));
-}
-gulp.task('client-build', ['react'], function () {
+};
+
+gulp.task('client-build', function () {
   return gulpClientBundle();
 });
 
